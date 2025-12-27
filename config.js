ce9c6be039e80b87c3294634c5e413e3998d7d0b
abdourefonte <abdou_ndiaye.refonte@ugb.edu.sn>
2025-12-27 13:10:00 +0000

    Auto-update: Token JWT renouvelé

---
+++ config.js
@@ -0,0 +1,16 @@
+// config.js - AUTO-GENERATED
+// Dernière mise à jour: 2025-12-27T13:10:00.738Z
+
+module.exports = {
+  getToken: () => {
+    return 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjYWlzc2Vfc2VuY3N1IiwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTc2NjkyNzQwMX0.fhjp7pw-BFMLNA4ATxTbh7OsM5iFkWHhwXeMZzaPZH-rB-JTpAiEvrCBCXRhxovtewMHLRU08lAraWIiZCZFJA';
+  },
+  
+  getTokenInfo: () => {
+    return {
+      lastUpdate: '2025-12-27T13:10:00.739Z',
+      tokenPreview: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjYWlzc2Vfc2VuY3N1I...',
+      expiresIn: '5 heures (prochaine mise à jour automatique)'
+    };
+  }
+};
