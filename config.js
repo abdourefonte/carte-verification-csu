599a3a777caed57f9ed6bd9e1dced0fa8d638b23
abdou.ndiaye <abdou_ndiaye.refonte@ugb.edu.sn>
2026-04-19 20:10:10 +0000

    Auto-update: Token JWT renouvelé

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 // config.js - AUTO-GENERATED
-// Dernière mise à jour: 2026-04-14T12:56:35.839Z
+// Dernière mise à jour: 2026-04-19T20:10:10.058Z
 
 module.exports = {
   getToken: () => {
-    return 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjYWlzc2Vfc2VuY3N1IiwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTc3NjI1Nzc5N30.zNqh5kclkV7uPpEcxkzfJt8JU1MFsPzpi3hh1dLyxMSDvH3p3x9XE4A7zIlSYccag2t3hvh5vLPiNxPJB-eNDQ';
+    return 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjYWlzc2Vfc2VuY3N1IiwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTc3NjcxNTgxM30.JO6GGXlj9eyMBnlI9gtcHd6U7S8vYUQCLqIXnsGcgByobMibysaXNzg-cMrmM46MIxLAZLOok2d9bTq9NmGkHA';
   },
   
   getTokenInfo: () => {
     return {
-      lastUpdate: '2026-04-14T12:56:35.839Z',
+      lastUpdate: '2026-04-19T20:10:10.060Z',
       tokenPreview: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjYWlzc2Vfc2VuY3N1I...',
       expiresIn: '5 heures (prochaine mise à jour automatique)'
     };
